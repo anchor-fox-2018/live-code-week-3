04a67349b62bed648be046d175ae338a81087d04
Lundy Orlando <simplyeazy@gmail.com>
2018-09-20 09:28:30 +0700

    Release 0

---
+++ oop/main.js
@@ -0,0 +1,21 @@
+class Day{
+    constructor(name){
+        this.name = name;
+        this.note = [];
+    }
+    static writeNote(note){
+        note.push(note);
+    }
+}
+
+class Diary {
+    constructor() {
+        this.month = month;
+        this.day = day;
+        this.diaries = [];
+
+    }
+}
+
+let hari = new Day("1", []);
+console.log(hari);
